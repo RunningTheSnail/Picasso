63ff89398789837523ffff6b591a27af1f6b9a3e
谭帅 <jerry@danwi.me>
2016-08-19 11:35:44 +0800

    init project

--- picasso/src/main/java/com/squareup/picasso/OkHttp3Downloader.java
+++ picasso/src/main/java/com/squareup/picasso/OkHttp3Downloader.java
@@ -22,6 +22,7 @@ import android.support.annotation.VisibleForTesting;
 
 import java.io.File;
 import java.io.IOException;
+
 import okhttp3.Cache;
 import okhttp3.CacheControl;
 import okhttp3.Call;
@@ -29,115 +30,124 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
 
-/** A {@link Downloader} which uses OkHttp to download images. */
+/**
+ * A {@link Downloader} which uses OkHttp to download images.
+ */
 public final class OkHttp3Downloader implements Downloader {
-  private final Call.Factory client;
-  private final Cache cache;
-  private boolean sharedClient = true;
+    private final Call.Factory client;
+    private final Cache cache;
+    private boolean sharedClient = true;
 
-  /**
-   * Create new downloader that uses OkHttp. This will install an image cache into your application
-   * cache directory.
-   */
-  public OkHttp3Downloader(final Context context) {
-    this(Utils.createDefaultCacheDir(context));
-  }
+    /**
+     * Create new downloader that uses OkHttp. This will install an image cache into your application
+     * cache directory.
+     */
+    public OkHttp3Downloader(final Context context) {
+        this(Utils.createDefaultCacheDir(context));
+    }
 
-  /**
-   * Create new downloader that uses OkHttp. This will install an image cache into the specified
-   * directory.
-   *
-   * @param cacheDir The directory in which the cache should be stored
-   */
-  public OkHttp3Downloader(final File cacheDir) {
-    this(cacheDir, Utils.calculateDiskCacheSize(cacheDir));
-  }
+    /**
+     * Create new downloader that uses OkHttp. This will install an image cache into the specified
+     * directory.
+     *
+     * @param cacheDir The directory in which the cache should be stored
+     */
+    public OkHttp3Downloader(final File cacheDir) {
+        this(cacheDir, Utils.calculateDiskCacheSize(cacheDir));
+    }
 
-  /**
-   * Create new downloader that uses OkHttp. This will install an image cache into your application
-   * cache directory.
-   *
-   * @param maxSize The size limit for the cache.
-   */
-  public OkHttp3Downloader(final Context context, final long maxSize) {
-    this(Utils.createDefaultCacheDir(context), maxSize);
-  }
+    /**
+     * Create new downloader that uses OkHttp. This will install an image cache into your application
+     * cache directory.
+     *
+     * @param maxSize The size limit for the cache.
+     */
+    public OkHttp3Downloader(final Context context, final long maxSize) {
+        this(Utils.createDefaultCacheDir(context), maxSize);
+    }
 
-  /**
-   * Create new downloader that uses OkHttp. This will install an image cache into the specified
-   * directory.
-   *
-   * @param cacheDir The directory in which the cache should be stored
-   * @param maxSize The size limit for the cache.
-   */
-  public OkHttp3Downloader(final File cacheDir, final long maxSize) {
-    this(new OkHttpClient.Builder().cache(new Cache(cacheDir, maxSize)).build());
-    sharedClient = false;
-  }
+    /**
+     * Create new downloader that uses OkHttp. This will install an image cache into the specified
+     * directory.
+     *
+     * @param cacheDir The directory in which the cache should be stored
+     * @param maxSize  The size limit for the cache.
+     */
+    public OkHttp3Downloader(final File cacheDir, final long maxSize) {
+        this(new OkHttpClient.Builder().cache(new Cache(cacheDir, maxSize)).build());
+        sharedClient = false;
+    }
 
-  /**
-   * Create a new downloader that uses the specified OkHttp instance. A response cache will not be
-   * automatically configured.
-   */
-  public OkHttp3Downloader(OkHttpClient client) {
-    this.client = client;
-    this.cache = client.cache();
-  }
+    /**
+     * Create a new downloader that uses the specified OkHttp instance. A response cache will not be
+     * automatically configured.
+     */
 
-  /** Create a new downloader that uses the specified {@link Call.Factory} instance. */
-  public OkHttp3Downloader(Call.Factory client) {
-    this.client = client;
-    this.cache = null;
-  }
+    //使用指定的OkHttpClient
+    public OkHttp3Downloader(OkHttpClient client) {
+        this.client = client;
+        this.cache = client.cache();
+    }
 
-  @VisibleForTesting Cache getCache() {
-    return ((OkHttpClient) client).cache();
-  }
+    /**
+     * Create a new downloader that uses the specified {@link Call.Factory} instance.
+     */
+    public OkHttp3Downloader(Call.Factory client) {
+        this.client = client;
+        this.cache = null;
+    }
 
-  @Override public Response load(@NonNull Uri uri, int networkPolicy) throws IOException {
-    CacheControl cacheControl = null;
-    if (networkPolicy != 0) {
-      if (NetworkPolicy.isOfflineOnly(networkPolicy)) {
-        cacheControl = CacheControl.FORCE_CACHE;
-      } else {
-        CacheControl.Builder builder = new CacheControl.Builder();
-        if (!NetworkPolicy.shouldReadFromDiskCache(networkPolicy)) {
-          builder.noCache();
+    @VisibleForTesting
+    Cache getCache() {
+        return ((OkHttpClient) client).cache();
+    }
+
+    @Override
+    public Response load(@NonNull Uri uri, int networkPolicy) throws IOException {
+        CacheControl cacheControl = null;
+        if (networkPolicy != 0) {
+            if (NetworkPolicy.isOfflineOnly(networkPolicy)) {
+                cacheControl = CacheControl.FORCE_CACHE;
+            } else {
+                CacheControl.Builder builder = new CacheControl.Builder();
+                if (!NetworkPolicy.shouldReadFromDiskCache(networkPolicy)) {
+                    builder.noCache();
+                }
+                if (!NetworkPolicy.shouldWriteToDiskCache(networkPolicy)) {
+                    builder.noStore();
+                }
+                cacheControl = builder.build();
+            }
         }
-        if (!NetworkPolicy.shouldWriteToDiskCache(networkPolicy)) {
-          builder.noStore();
+
+        Request.Builder builder = new okhttp3.Request.Builder().url(uri.toString());
+        if (cacheControl != null) {
+            builder.cacheControl(cacheControl);
         }
-        cacheControl = builder.build();
-      }
-    }
 
-    Request.Builder builder = new okhttp3.Request.Builder().url(uri.toString());
-    if (cacheControl != null) {
-      builder.cacheControl(cacheControl);
-    }
-
-    okhttp3.Response response = client.newCall(builder.build()).execute();
-    int responseCode = response.code();
-    if (responseCode >= 300) {
-      response.body().close();
-      throw new ResponseException(responseCode + " " + response.message(), networkPolicy,
-          responseCode);
-    }
-
-    boolean fromCache = response.cacheResponse() != null;
-
-    ResponseBody responseBody = response.body();
-    return new Response(responseBody.byteStream(), fromCache, responseBody.contentLength());
-  }
-
-  @Override public void shutdown() {
-    if (!sharedClient) {
-      if (cache != null) {
-        try {
-          cache.close();
-        } catch (IOException ignored) {
+        okhttp3.Response response = client.newCall(builder.build()).execute();
+        int responseCode = response.code();
+        if (responseCode >= 300) {
+            response.body().close();
+            throw new ResponseException(responseCode + " " + response.message(), networkPolicy,
+                    responseCode);
         }
-      }
+
+        boolean fromCache = response.cacheResponse() != null;
+
+        ResponseBody responseBody = response.body();
+        return new Response(responseBody.byteStream(), fromCache, responseBody.contentLength());
     }
-  }
+
+    @Override
+    public void shutdown() {
+        if (!sharedClient) {
+            if (cache != null) {
+                try {
+                    cache.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
 }
